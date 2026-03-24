500624fa6aeb1aa7a12410675869e067037933b1
miquel06 <miquelfontana08@gmail.com>
2026-03-24 08:55:10 +0100

    maquetación home app barberia

---
+++ app_mobil/IceColdApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace IceColdApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
