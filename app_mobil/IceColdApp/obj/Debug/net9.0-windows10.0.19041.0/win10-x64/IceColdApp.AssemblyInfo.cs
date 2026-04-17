21d711077afc5644bfbb2592fdc57ef1c2aa7007
miquel06 <miquelfontana08@gmail.com>
2026-04-17 09:48:10 +0200

    Añadido contorno azul a los servicios en ServiciosPage
    - Se han envuelto los elementos de cada servicio en un control Border con un contorno azul (#2196F3) y esquinas redondeadas.
    
    - Se han eliminado los BoxView que hacían de líneas de separacion
    
    - Se han reajustado los márgenes (Margin) y rellenos (Padding) para separar correctamente las tarjetas entre sí

--- app_mobil/IceColdApp/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/IceColdApp.AssemblyInfo.cs
+++ app_mobil/IceColdApp/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/IceColdApp.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IceColdApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+af8f428e3c12289fd985e838e37186df08026a7a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+b698b25d21395b69bc0c0c0095ae8741bbf2161b")]
 [assembly: System.Reflection.AssemblyProductAttribute("IceColdApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IceColdApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
