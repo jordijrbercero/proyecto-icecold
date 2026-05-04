06200e0e3ca422f6d88214dbb3636040e1d9ecd0
jordi <jordijrbercero@gmail.com>
2026-05-04 12:22:03 +0200

    IceColdAdmin

--- app_mobil/IceColdApp/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/IceColdApp.AssemblyInfo.cs
+++ app_mobil/IceColdApp/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/IceColdApp.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IceColdApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+123369ef30010841ef8f0822c536c5d508ab31bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+a50a7838f11571ae6b9e60c87fbce4ecb314a0fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("IceColdApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IceColdApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
